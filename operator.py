d340e434caeaf11d5176e54219171149adba3e73
Unmesh Das <unmeshdas57@gmail.com>
2025-12-24 23:03:56 +0600

    new

--- operator.py
+++ operator.py
@@ -1,6 +1,6 @@
 ##all operator
 
-##[1]terminary operator
+##[1]terminary operator(Special)
 """x=int(input("enter your number: "))
 result= "Even" if x % 2 == 0 else "Odd"
 print(result)"""
@@ -39,12 +39,46 @@ print(a>2 or b<10)
 print(not(a<2))
 '''
 
+##[5]Assignment Operator
+'''
+a=10
+b=5
+##a+=2
+##b-=10
+##a**=10
+##b/=10
 
+print(a)
+print(b)
+'''
 
+##[6]Bitwise Operator
+'''
+a=5
+b=10
+print(a | b)
+print(a&b)
+print (a^b)
+print(a<<1)
+print(b>>1)
+'''
 
+##[7]Membership Operator
+'''
+a=[1,2,3]
+print(2 in a)
+print(2 not in a)
+'''
 
-
-
+##[8]Identity Operator
+'''
+a=[1,2]
+b=a 
+c=[3]
+print(a is b)
+print(c is a)
+print(c is not a)
+'''
 
 
 
