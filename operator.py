0745bff8e85664306cf04bc3af3d8b74de76ca1a
Unmesh Das <unmeshdas57@gmail.com>
2025-12-24 22:07:58 +0600

    new

--- operator.py
+++ operator.py
@@ -1,3 +1,56 @@
-x=int(input("enter your number: "))
+##all operator
+
+##[1]terminary operator
+"""x=int(input("enter your number: "))
 result= "Even" if x % 2 == 0 else "Odd"
-print(result)
+print(result)"""
+
+##[2]Arithmatic opetator
+'''
+a=10
+b=20
+print(a+b)
+print(b-a)
+print(b/10)
+print(b%a)
+print(a*b)
+print(a//b)
+print(a**b)
+'''
+
+##[3]relational operator
+'''
+a=10
+b=3
+print(a==b)
+print(a>b)
+print(a>=b)
+print(a<b)
+print(a<=b)
+print(a!=b)
+'''
+
+##[4]Logical Operator
+'''
+a=10
+b=50
+print(a>2 and b<10)
+print(a>2 or b<10)
+print(not(a<2))
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
