edcc36360270355218107e0c83b822a24fa5f9fe
Unmesh Das <unmeshdas57@gmail.com>
2025-12-24 23:53:20 +0600

    new

---
+++ control.py
@@ -0,0 +1,49 @@
+##[1] if-else
+'''
+x=3
+if x>4:
+  print("hi")
+else:
+  print("bye")
+'''
+
+##[2] if-elif-else
+'''
+marks=int(input("enter your mark: "))
+if marks>80 | marks<100:
+  print("A+")
+elif marks>70 | marks<80:
+  print("A")
+elif marks>60 | marks<70:
+  print("B")
+elif marks>50 | marks<60:
+  print("C")
+else:
+  print("F")
+'''
+
+##[3]Nested if
+'''
+x=10
+if x>5:
+  if x<20:
+    print("hi")
+'''
+
+##[4]Match Case
+'''
+m=2
+match m:
+  case 1:
+    print("sunday")
+  case 2:
+    print("monday")
+  case _:
+    print("invalid")
+'''
+
+
+
+
+
+
