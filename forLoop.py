56d39043075ef17359cd1e47edfdb659123cb7c0
Unmesh Das <unmeshdas57@gmail.com>
2025-12-25 22:20:17 +0600

    new

---
+++ forLoop.py
@@ -0,0 +1,60 @@
+###Statement:
+'''
+for i in range(start,stop,step):
+  print i
+'''
+
+##[1]
+'''
+for i in range(2,6,2):
+  print (i)
+'''
+
+##[2]
+'''
+fruits=["apple","banana","mango"]
+for item in fruits:
+  print(item)
+'''
+
+##Nested Loop Structure
+'''
+for i in sequence:
+  for j in sequence:
+    statement(s)
+'''
+
+##[3]
+'''
+for i in range(1,3):
+  for j in range(1,4):
+    print("*")
+'''
+
+##[4] for loop with break
+'''
+for i in range(5):
+  if i==3:
+    break
+  print(i)
+'''
+
+##[5] for loop with continue
+'''
+for i in range(5):
+  if i==3:
+    continue
+  print(i)
+'''
+
+##[6]for loop with pass(empty loop for future)
+'''
+for i in range(3):
+
+  pass
+'''
+
+
+
+
+
---
+++ forLoopPyramid.py
@@ -0,0 +1,41 @@
+##[1]
+'''
+n = 5
+for i in range(1, n+1):
+    for j in range(i):
+        print("*", end="")
+    print()
+'''
+
+##[2]
+'''
+n=5
+for i in range(n,0,-1):
+  for j in range(i):
+     print("*",end="")
+  print()
+'''
+
+##[3]
+'''
+n=5
+for i in range(1,n+1):
+  print(" "*(n-i)+"*"*(2*i-1))
+'''
+
+##[4]
+'''
+n=5
+for i in range(n,0,-1):
+  print(" "*(n-i)+"*"*(2*i-1))
+'''
+
+##[5]
+'''
+n = 5
+for i in range(1, n+1):
+    for j in range(1,i+1):
+        print(j, end="")
+    print()
+'''
+
